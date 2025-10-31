d44a55619edeb670032854f6dc161f5abb522866
Saikat-Mahmud <eshikat355@rku.ac.in>
2025-11-01 00:56:38 +0600

    about page update

--- src/Pages/UserProfile/Components/ProfileAbout.jsx
+++ src/Pages/UserProfile/Components/ProfileAbout.jsx
@@ -4,9 +4,14 @@ export default function ProfileAbout({ user }) {
       <h3 className="text-xl font-semibold mb-4 text-gray-700">About {user.name}</h3>
       <ul className="space-y-2 text-gray-700">
         <li><b>Email:</b> {user.email}</li>
+        <li><b>Graduated From:</b> {user.school || "Not specified"}</li>
+        <li><b>Address:</b> {user.address || "Not specified"}</li>
         <li><b>Country:</b> {user.country || "Not specified"}</li>
         <li><b>Joined:</b> {new Date(user.created_at).toLocaleDateString()}</li>
-        <li><b>Bio:</b> {user.bio || "No bio added yet."}</li>
+        <li><b>Bio:</b> {user.bio == null && "No bio added yet."}</li>
+        {user.bio != null &&
+          <p>{user.bio}</p>
+        }
       </ul>
     </div>
   );
